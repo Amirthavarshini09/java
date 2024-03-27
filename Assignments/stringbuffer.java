f96ee7e4d06ac2c7c4bc14972fbc4cf326290634
Amirthavarshini09 <amirthaarun49@gmail.com>
2024-03-27 11:50:51 +0530

    my first origin

---
+++ Assignments/stringbuffer.java
@@ -0,0 +1,91 @@
+package day3;
+
+public class stringbuffer {
+	
+public static void main (String[] args) {
+//	StringBuffer sc = new StringBuffer();
+//	StringBuffer sB = new StringBuffer(23);
+//	StringBuffer sB1 = new StringBuffer("HIaallllll");
+//	StringBuffer sB2= new StringBuffer();
+//	System.out.println(sc.capacity());
+//	System.out.println(sB.capacity());
+//	System.out.println(sB1.capacity());
+//	System.out.println(sB2.capacity());
+//	System.out.println("***********************");
+//	System.out.println(sc.length());
+//	System.out.println(sB.length());
+//	System.out.println(sB1.length());
+//	System.out.println(sB2.length());
+//	
+//	System.out.println(sB1.append("haiii"));
+//	System.out.println(sB1);
+//	sB1.insert(2,"hi");
+//	System.out.println(sc);
+//	sB1.deleteCharAt(3);
+//	System.out.println(sB1);
+//	
+//
+//String st= new String("Alphabey");
+//StringBuffer st1= new StringBuffer("Alphabey");
+//String result1 =st.substring(3,8);
+//System.out.println(result1);
+	
+//	StringBuffer sb = new StringBuffer("Able was I ere I saw Elba.");
+//	System.out.println(sb.capacity());
+//	
+//	
+//	String hannah = "Did Hannah see bees? Hannah did.";
+//	System.out.println(hannah.length());
+//	
+//	hannah.charAt(12);
+//	System.out.println(hannah.charAt(15));
+//	
+//	String A= ("Was it a car or a cat I saw?");
+//			String i = A.substring(9, 12);
+//	System.out.println(i);
+	
+	
+	
+	
+//	
+//	
+//	public class ComputeResult {
+//
+//		public static void main(String[] args) {
+//
+////		String original = "software";
+////
+////		StringBuffer result = new StringBuffer("hi");
+////
+////		int index = original.indexOf('a');
+////
+////		 result.setCharAt(0, original.charAt(0));
+////
+////		 result.setCharAt(1, original.charAt(original.length()-1));
+////		 
+////		 result.insert(1, original.charAt(4));
+////
+////		 result.append(original.substring(1,4));
+////
+////		 result.insert(3, (original.substring(index, index+2) + " "));
+////
+////		System.out.println(result);
+////		}
+
+			StringBuffer hi = new StringBuffer("Hi, ");
+
+			StringBuffer mom = new StringBuffer("mom.");
+			 
+//			 hi.insert(4,"mom");
+			 hi.append("mom");
+			 
+			 System.out.println(hi);
+		
+		
+	
+
+
+}
+}
+
+
